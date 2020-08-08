83719158e0e655fc84d1cd3e859dd288eab81702
st0n1 <github@steinc.de>
2020-08-08 16:06:28 +0200

    set pwsh.exe as variable

--- NASBackup_Flashblade/Run-FlashbladeNASBackup.ps1
+++ NASBackup_Flashblade/Run-FlashbladeNASBackup.ps1
@@ -1,12 +1,15 @@
 <#
    .SYNOPSIS
-   Wrapper to run Invoke-FlashbladeNASBackup.ps1 with pwsh and prefill required arguments
+   Wrapper to run Invoke-FlashbladeNASBackup.ps1 with pwsh and prefill required arguments.
+   Wrapper calls static Powershell path as a Veeam requirement. Change it as needed
    .Notes 
    Version:        1.0
    Author:         Christian Stein
    Creation Date:  08.08.2020
 
 #>
+
+$PWSHEXE = "C:\Program Files\PowerShell\7\pwsh.exe"
 
 $arguments = @{
     #
@@ -32,11 +35,4 @@ $arguments = @{
  }
  # 
 
-$PSVersionTable.PSVersion | Out-Null
-
-if ($PSVersionTable.PSVersion -lt "8.0")
-{
-   & C:\"Program Files"\PowerShell\7\pwsh.exe -f ./Invoke-FlashbladeNASBackup.ps1 @arguments
-   return
-   # & pwsh -f ./Invoke-FlashbladeNASBackup.ps1 @arguments
-}
+& "$PWSHEXE" -f ./Invoke-FlashbladeNASBackup.ps1 @arguments
